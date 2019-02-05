009c804091382291b556e5fb69d5bf864a30aa0e
Will Skora <skorasaurus@gmail.com>
2019-02-05 18:23:50 -0500

    add functions

--- functions.php
+++ functions.php
@@ -1,9 +1,22 @@
-
-
 <?php
+
+
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
-    wp_enqueue_style( 'twentyninteen', get_template_directory_uri() . '/style.css' );
- 
+    wp_enqueue_style( 'quark', get_template_directory_uri() . '/style.css' );
 }
+
+function my_theme_setup() {
+    // Lots of other code …
+
+    // Enqueue editor styles.
+    add_editor_style( 'style-editor.css' );
+}
+add_action( 'after_setup_theme', 'my_theme_setup' );
+
+// add support for gutenberg 
+// https://deliciousbrains.com/preparing-wordpress-themes-gutenberg/
+
+
+
 ?>
