1e2a00c5d0b9ed81b0534dcb9322c480129e557d
will skora <skorasaurus@gmail.com>
2019-05-04 17:26:39 -0400

    enqueue local style file

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
-    wp_enqueue_style( 'quark', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'roland', get_template_directory_uri() . '/style.css' );
 }
 
 function my_theme_setup() {
@@ -16,6 +16,8 @@ add_action( 'after_setup_theme', 'my_theme_setup' );
 
 // add support for gutenberg 
 // https://deliciousbrains.com/preparing-wordpress-themes-gutenberg/
+
+// add_theme_support( 'editor-styles' );
 
 
 
